995a85e252f58dbb267bc20e49e57437b783f62a
MoVehrs <moritz.vehrs@web.de>
2025-06-05 21:59:26 +0200

    Updated doxygen

--- direct_skills/doxygen/html/navtreedata.js
+++ direct_skills/doxygen/html/navtreedata.js
@@ -28,24 +28,29 @@ var NAVTREE =
     [ "DirectSkills Plugin for RPG Maker 2003 (DynRPG)", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html", [
       [ "Features", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md1", null ],
       [ "Installation", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md2", null ],
-      [ "Technical Details", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md3", [
-        [ "Value Ranges and Limitations", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md4", null ],
-        [ "Important Requirements", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md5", null ],
-        [ "Error Handling", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md6", null ],
-        [ "Debug Output Format", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md7", null ]
+      [ "Setup Requirements", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md3", [
+        [ "Battle Event Setup", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md4", null ],
+        [ "Common Event Setup", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md5", null ]
       ] ],
-      [ "Configuration", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md8", [
-        [ "Debug Options", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md9", null ],
-        [ "Battle Command Mappings", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md10", [
-          [ "Direct Skill Mapping", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md11", null ],
-          [ "Variable-Based Mapping", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md12", null ]
+      [ "Technical Details", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md6", [
+        [ "Skill Target Limitations", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md7", null ],
+        [ "Value Ranges and Limitations", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md8", null ],
+        [ "Important Requirements", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md9", null ],
+        [ "Error Handling", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md10", null ],
+        [ "Debug Output Format", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md11", null ]
+      ] ],
+      [ "Configuration", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md12", [
+        [ "Debug Options", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md13", null ],
+        [ "Battle Command Mappings", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md14", [
+          [ "Direct Skill Mapping", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md15", null ],
+          [ "Variable-Based Mapping", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md16", null ]
         ] ],
-        [ "Error Handling Examples", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md13", null ],
-        [ "Debug Output Examples", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md14", null ]
+        [ "Error Handling Examples", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md17", null ],
+        [ "Debug Output Examples", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md18", null ]
       ] ],
-      [ "Compatibility with limit_break Plugin", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md15", null ],
-      [ "Troubleshooting", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md16", null ],
-      [ "Credits", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md17", null ]
+      [ "Compatibility with limit_break Plugin", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md19", null ],
+      [ "Troubleshooting", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md20", null ],
+      [ "Credits", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2direct__skills_2_r_e_a_d_m_e.html#autotoc_md21", null ]
     ] ],
     [ "Topics", "topics.html", "topics" ],
     [ "Namespaces", "namespaces.html", [

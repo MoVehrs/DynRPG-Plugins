c7e265b71f434bff48757edb0c62b27e89629133
MoVehrs <moritz.vehrs@web.de>
2025-06-05 21:57:38 +0200

    Updated doxygen

--- bare_handed/doxygen/html/navtreedata.js
+++ bare_handed/doxygen/html/navtreedata.js
@@ -48,8 +48,11 @@ var NAVTREE =
         [ "2. Update Bare Hand Equipment", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2bare__handed_2_r_e_a_d_m_e.html#autotoc_md17", null ],
         [ "Important: Using the Change Equipment Event Command", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2bare__handed_2_r_e_a_d_m_e.html#autotoc_md18", null ]
       ] ],
-      [ "Troubleshooting", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2bare__handed_2_r_e_a_d_m_e.html#autotoc_md19", null ],
-      [ "Credits", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2bare__handed_2_r_e_a_d_m_e.html#autotoc_md20", null ]
+      [ "Combat System and Formulas", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2bare__handed_2_r_e_a_d_m_e.html#autotoc_md19", [
+        [ "Default vs Plugin Combat", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2bare__handed_2_r_e_a_d_m_e.html#autotoc_md20", null ]
+      ] ],
+      [ "Troubleshooting", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2bare__handed_2_r_e_a_d_m_e.html#autotoc_md21", null ],
+      [ "Credits", "md__c_1_2_users_2_buddy_2_desktop_2_dyn_r_p_g_01_test_2_dyn_plugins_2bare__handed_2_r_e_a_d_m_e.html#autotoc_md22", null ]
     ] ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
